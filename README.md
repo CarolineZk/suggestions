b3c60f0b36fbda68cd8644df342f8f3f917e6c3a
CarolineZk <c.moreau.zow@gmail.com>
2018-02-13 22:25:17 -0500

    'Init'

---
+++ README.md
@@ -0,0 +1,5 @@
+# suggestions
+
+Front-end app to connect to [Suggestions API](https://voobanchallenge.azurewebsites.net/swagger/ui/index)
+
+Using Vue.js + Vuetify + Ajax
---
+++ app.js
@@ -0,0 +1,83 @@
+// Call Ajax
+function ajaxGet(url, callback) {
+    var req = new XMLHttpRequest();
+    req.open('GET', url, true);
+    req.addEventListener('load', function () {
+        if (req.status >= 200 && req.status < 400) {
+            callback(req.responseText);
+        } else {
+            console.error(req.status + ' ' + req.statusText + ' ' + url);
+        }
+    });
+    req.addEventListener('error', function () {
+        console.error('Network error ' + url);
+    });
+    req.send(null);
+}
+
+// Set vars
+var resultsVal = document.getElementById('resultsCt');
+var resultsTableVal = document.getElementById('resultsTable');
+var totalVal = document.getElementById('totalCt');
+var cityVal = document.getElementById('city');
+var pageNb = 1;
+var itemsNb = 15;
+
+// Display prev/next buttons
+function btnOn() {
+    document.getElementById('previousBtn').classList.remove('btn--dis');
+    document.getElementById('nextBtn').classList.remove('btn--dis');
+}
+function btnOff() {
+    document.getElementById('previousBtn').classList.add('btn--dis');
+    document.getElementById('nextBtn').classList.add('btn--dis');
+}
+
+// Get the data
+function getData(cityReq, pageNb, itemsNb) {
+    resultsTableVal.innerHTML = '';
+    totalVal.innerHTML = '';
+
+    ajaxGet('https://voobanchallenge.azurewebsites.net/suggestions?q=' + cityReq + '&pageNumber=' + pageNb + '&pageSize=' + itemsNb, function(response) {
+        var obj = JSON.parse(response);
+        var objTotal = obj.TotalItems;
+        var pageTotal = objTotal/itemsNb;
+
+        if (objTotal > 0) {
+            btnOn();
+            totalVal.innerHTML = 'Total: ' + objTotal;
+            for (i = 0; i < objTotal; i++) {
+                if (typeof obj.Items[i] !== "undefined") {
+                    resultsTableVal.innerHTML +='<tr><td>' + obj.Items[i].Name + '</td><td>' + obj.Items[i].Latitude + '</td><td>' + obj.Items[i].Longitude + '</td><td>' + obj.Items[i].Score + '</td></tr>';
+                }
+            }
+        } else {
+            totalVal.innerHTML = 'No result found';
+            btnOff();
+        }
+    });
+};
+
+// Display results after the 3rd char in the search bar
+cityVal.addEventListener('keyup', function() {
+    var cityReq = cityVal.value;
+    if (cityReq.length < 3) {
+        resultsTableVal.innerHTML = '';
+        totalVal.innerHTML = '';
+    } else {
+        getData(cityReq, 1, 15);
+    }
+});
+
+// Display prev/next pages results by clicking the buttons
+nextBtn.addEventListener('click', function() {
+    ++pageNb;
+    var cityReq = cityVal.value;
+    getData(cityReq, pageNb, 15);
+});
+
+previousBtn.addEventListener('click', function() {
+    --pageNb;
+    var cityReq = cityVal.value;
+    getData(cityReq, pageNb, 15);
+});
