65b807d26a6823de902d45c304f6384622e9289c
Caroline Zowczak <czowczak@ovh-que-czowc.local>
2018-02-16 14:18:41 -0500

    fix(table): reset counter + add transition

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ var resultsVal = document.getElementById('resultsCt');
 var resultsTableVal = document.getElementById('resultsTable');
 var totalVal = document.getElementById('totalCt');
 var cityVal = document.getElementById('city');
+var cityReq = '';
 var pageNb = 1;
 var itemsNb = 15;
 
@@ -28,6 +29,7 @@ function btnOn() {
     document.getElementById('previousBtn').classList.remove('btn--dis');
     document.getElementById('nextBtn').classList.remove('btn--dis');
 }
+
 function btnOff() {
     document.getElementById('previousBtn').classList.add('btn--dis');
     document.getElementById('nextBtn').classList.add('btn--dis');
@@ -38,20 +40,21 @@ function getData(cityReq, pageNb, itemsNb) {
     resultsTableVal.innerHTML = '';
     totalVal.innerHTML = '';
 
-    ajaxGet('https://voobanchallenge.azurewebsites.net/suggestions?q=' + cityReq + '&pageNumber=' + pageNb + '&pageSize=' + itemsNb, function(response) {
+    ajaxGet('https://voobanchallenge.azurewebsites.net/suggestions?q=' + cityReq + '&pageNumber=' + pageNb + '&pageSize=' + itemsNb, function (response) {
         var obj = JSON.parse(response);
         var objTotal = obj.TotalItems;
         var pageTotal = objTotal/itemsNb;
-
         if (objTotal > 0) {
-            btnOn();
-            totalVal.innerHTML = 'Total: ' + objTotal;
             for (i = 0; i < objTotal; i++) {
-                if (typeof obj.Items[i] !== "undefined") {
+                if (typeof obj.Items[i] !== 'undefined') {
                     resultsTableVal.innerHTML +='<tr><td>' + obj.Items[i].Name + '</td><td>' + obj.Items[i].Latitude + '</td><td>' + obj.Items[i].Longitude + '</td><td>' + obj.Items[i].Score + '</td></tr>';
                 }
             }
+            resultsVal.classList.add('visible');
+            totalVal.innerHTML = 'Total: ' + objTotal;
+            btnOn();
         } else {
+            resultsVal.classList.remove('visible');
             totalVal.innerHTML = 'No result found';
             btnOff();
         }
@@ -59,25 +62,27 @@ function getData(cityReq, pageNb, itemsNb) {
 };
 
 // Display results after the 3rd char in the search bar
-cityVal.addEventListener('keyup', function() {
-    var cityReq = cityVal.value;
+cityVal.addEventListener('keyup', function () {
+    cityReq = cityVal.value;
+    pageNb = 1;
     if (cityReq.length < 3) {
+        resultsVal.classList.remove('visible');
         resultsTableVal.innerHTML = '';
         totalVal.innerHTML = '';
+        btnOff();
     } else {
-        getData(cityReq, 1, 15);
+        getData(cityReq, 1, itemsNb);
+        btnOn();
     }
 });
 
 // Display prev/next pages results by clicking the buttons
-nextBtn.addEventListener('click', function() {
-    ++pageNb;
-    var cityReq = cityVal.value;
-    getData(cityReq, pageNb, 15);
+nextBtn.addEventListener('click', function () {
+    cityReq = cityVal.value;
+    getData(cityReq, ++pageNb, itemsNb);
 });
 
-previousBtn.addEventListener('click', function() {
-    --pageNb;
-    var cityReq = cityVal.value;
-    getData(cityReq, pageNb, 15);
+previousBtn.addEventListener('click', function () {
+    cityReq = cityVal.value;
+    getData(cityReq, --pageNb, itemsNb);
 });
